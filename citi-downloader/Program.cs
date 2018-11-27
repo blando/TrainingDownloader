be2ab0f41a9cfd606c65cc1e339a79c6254cd2a4
Mark Bland <mbland@iastate.edu>
2018-11-26 21:04:10 -0600

    Add project files.

---
+++ citi-downloader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace citi_downloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
